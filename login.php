0dedc48b7e0b4ad982c1d408c9afd1db842a5e2f
willam D <prakritkadari5@gmail.com>
2022-10-25 15:20:52 +0545

    Add login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Login Form</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <div>
+        <input type="text" placeholder="enter your email" name="email">
+        <input type="text" placeholder="enter your password" name="password">
+        <button>Submit</button>
+    </div>
+	
+
+
+	<footer>
+			<div class="container">
+				Copyright &copy; IT SERIES TUTOR
+			</div>
+
+
+	</footer>
+
+</body>
+</html>
