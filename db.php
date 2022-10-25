9f77f0b082f1680bb4cc084d064413171cb14932
willam D <prakritkadari5@gmail.com>
2022-10-25 15:15:31 +0545

    Add quiz file in my quiz repo

---
+++ db.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+<?php
+	$dbhost	= "localhost";
+	 $dbuser = "root";
+	 $dbpass = "";
+	$dbname = "quiz";
+
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+
+	if(mysqli_connect_errno()){ 
+		die("Database Connection Failed" . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")");
+	}
+
+?>
+</head>
+<body></body>
+</html>
